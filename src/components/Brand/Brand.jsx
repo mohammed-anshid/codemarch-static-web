e8fed0c45306a603e2f718156cddb09396698331
Anxhid-Mohed <mohammedanshid100@gmail.com>
2023-10-09 22:46:50 +0530

    final

--- src/components/Brand/Brand.jsx
+++ src/components/Brand/Brand.jsx
@@ -45,12 +45,12 @@ function Brand() {
     return () => clearInterval(interval);
   }, []);
   return (
-    <section className="container px-[1rem] md:px-[2rem] justify-items-center justify-center mx-auto lg:pt-7">
+    <section className="container px-[1rem] md:px-[2rem] justify-items-center justify-center mx-auto ">
       <div className="cards">
         {
           isMdScreenOrLarger ? 
           (
-            <div className="w-fit mx-auto grid grid-cols-3 lg:grid-cols-3 md:grid-cols-3 md:justify-items-center md:justify-center gap-x-72 md:gap-x-11 mb-5 pt-40 overflow-y-hidden overflow-x-auto">
+            <div className="w-fit mx-auto grid grid-cols-3 lg:grid-cols-3 md:grid-cols-3 md:justify-items-center md:justify-center gap-x-72 md:gap-x-11 mb-5 pt-28 overflow-y-hidden overflow-x-auto">
             {cards?.map((card,index) => {
               return (
                 <div className="flex flex-col items-center relative">
@@ -82,7 +82,7 @@ function Brand() {
           Master coding, build your brand & land your dream job. <br /> Launch your coding career with confidence
         </h3>
         <button className="btn flex mx-auto items-center text-center font-bold text-[#01221C] md:text-[1.3rem] bg-gradient-to-b from-brandText to-brandHover w-full md:w-56 h-10 md:h-14 justify-center mt-6 md:mt-9 lg:mt-12 rounded-[9px]">
-          Soon<span className="ml-1 md:ml-[4px] text-xs md:text-[1.1rem]"><FaArrowRight/></span>
+          coming soon<span className="ml-1 md:ml-[4px] text-xs md:text-[1.1rem]"><FaArrowRight/></span>
         </button>
       </div>
     </section>
