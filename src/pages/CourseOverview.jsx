ee85d5d1869630ab06814ee85256b75f1a6b948e
Anxhid-Mohed <mohammedanshid100@gmail.com>
2024-01-01 16:43:11 +0530

    changes

--- src/pages/CourseOverview.jsx
+++ src/pages/CourseOverview.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import NavBar from '../components/NavBar/NavBar'
 import Footer from '../components/Footer/Footer'
 import Review from '../components/Review/review'
@@ -10,7 +10,9 @@ import FAQ from '../components/FAQ/FAQ'
 
 const CourseOverview = () => {
     const data =[ {heading:"What is JavaScript?",text:"JavaScript is mainly used to add animations and interactive elements to websites, but there are libraries and frameworks that extend its use into mobile development, game development, and more. You can even use it to build virtual reality applications and artificial intelligence."},  {heading:"What does JavaScript do?",text:"JavaScript is mainly used to add animations and interactive elements to websites, but there are libraries and frameworks that extend its use into mobile development, game development, and more. You can even use it to build virtual reality applications and artificial intelligence."},{heading:"What kind of Jobs can JavaScript get me?",text:"JavaScript is mainly used to add animations and interactive elements to websites, but there are libraries and frameworks that extend its use into mobile development, game development, and more. You can even use it to build virtual reality applications and artificial intelligence."},{heading:"Why is JavaScript so popular?",text:"JavaScript is mainly used to add animations and interactive elements to websites, but there are libraries and frameworks that extend its use into mobile development, game development, and more. You can even use it to build virtual reality applications and artificial intelligence."},{heading:"Are Java & JavaScript the same?",text:"JavaScript is mainly used to add animations and interactive elements to websites, but there are libraries and frameworks that extend its use into mobile development, game development, and more. You can even use it to build virtual reality applications and artificial intelligence."},]
-  
+    useEffect(() => {
+        window.scrollTo(0, 0);
+    }, []);
   return (
     <>
         <NavBar/>
